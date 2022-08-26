8be2778aac89c47272723d76e770db69e18b6ba8
eugene <uwm1004@gmail.com>
2022-08-26 22:16:35 +0900

    mobx

---
+++ src/components/TodoDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+function TodoOverview() {
+  return (
+    <>
+      <TodoForm />
+      <TodoList />
+    </>
+  );
+}
+export default TodoOverview;
---
+++ src/store/TodoStore.ts
@@ -0,0 +1,7 @@
+import { makeAutoObservable } from "mobx";
+const todoStore = () => {
+  return makeAutoObservable({
+    list: [] as { title: string; id: number }[],
+  });
+};
+export default todoStore;
